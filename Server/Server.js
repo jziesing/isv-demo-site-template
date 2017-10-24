3b4c649aa95025b5bbf1caba26bd42216cdc6d34
Jack Ziesing <jack.ziesing@jziesing-ltm.internal.salesforce.com>
2017-10-24 16:44:19 -0700

    adding register call

--- Server/Server.js
+++ Server/Server.js
@@ -12,8 +12,8 @@ require('babel-register')({
 let express = require('express'),
 	bodyParser = require('body-parser'),
 	apiRouter = require('./ApiRouter.js'),
-	clientRouter = require('./ClientRouter.js');
-
+	clientRouter = require('./ClientRouter.js'),
+    ajax = require('superagent');
 
 
 //  create server app
@@ -32,5 +32,32 @@ app.use(express.static(__dirname + '/../Client/build/'));
 app.use(clientRouter);
 
 
+// register app function
+function RegisterApp() {
+
+    let dataToSend = {
+        "appURL": process.env.appURL,
+        "appName": process.env.appName,
+        "companyId": process.env.companyId,
+        "contactEmail": process.env.contactEmail
+    };
+
+    ajax.post('http://partner-site-template-reporter.herokuapp.com/jzapi/register-app')
+        .set({ 'jzapi-token': 'change_this_later' })
+        .send(dataToSend)
+        .end((error, resp) => {
+            console.log(resp);
+            if(!error && resp.status == 200) {
+                console.log('success register call : ' + resp);
+            } else {
+                console.log('error register call : ' + resp);
+            }
+        });
+}
+
+
 //  run
-app.listen(port, () => console.log( "Express server listening on port " + port) );
+app.listen(port, () => {
+    RegisterApp();
+    console.log( "Express server listening on port " + port);
+});
